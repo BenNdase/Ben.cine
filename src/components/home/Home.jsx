3c00e3379a4cbebbaaa2ae4537125e71a64020ef
Benjamin Tondase <benjamin1tondase211@gmail.com>
2021-05-07 12:58:41 +0100

    feat : create cards for well-noted movies

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,41 +1,73 @@
 import React, { useEffect, useState } from "react";
 import "./Home.scss"
-import { RecoverMovies, RecoverMoviesByCategory, RecoverTopratedMovies } from "../../service/api";
+import { RecoverMovies, RecoverPopularMovies , RecoverTopratedMovies } from "../../service/api";
 import Header from "../header/Header";
 import CardMovies from "../card/Card";
 import {Link}from "react-router-dom"
 
 const Home = () => {
   const [nowPLaying, setNowPlaying] = useState([]);
-  const [topratedMovies, setTopratedMovies] = useState([])
-  const [movieByCategory, setMoviesByCategory] = useState([])
+  const [topratedMovies, setTopratedMovies] = useState([]);
+  const [popularMovies, setPopularMovies] = useState([]);
+
   useEffect(() => {
     const fetchApi = async () => {
       setNowPlaying(await RecoverMovies());
-      setMoviesByCategory(await RecoverMoviesByCategory());
       setTopratedMovies(await RecoverTopratedMovies());
+      setPopularMovies(await RecoverPopularMovies());
     };
     fetchApi();
   }, []);
+
   const tabMovies = [];
-  const moviesPopularList = nowPLaying.slice(0, 9).map((movie,index) => {
+  const moviesNowPlaying = nowPLaying.slice(0, 9).map((movie,index) => {
     tabMovies.push(movie.backPoster);
     return(
         <>
-            {index <= 6 && index !== 4 ? <CardMovies key={index} id={movie.id} poster={movie.poster} title={movie.title} rating={movie.rating} /> : null}
+            {index <=5 ? <CardMovies key={index} id={movie.id} poster={movie.poster} title={movie.title} rating={movie.rating} /> : null}
         </>
     )
   });
+
   const moviesTopratedList = topratedMovies.slice(0,6).map((movie,index) => {
     return(
       <>
         <CardMovies key={index} id={movie.id} poster={movie.poster} title={movie.title} rating={movie.rating} /> 
       </>
     )
-  })
+  });
+
+  const moviesPopularList = popularMovies.slice(0,6).map((movie,index) => {
+    return(
+      <>
+        <CardMovies key={index} id={movie.id} poster={movie.poster} title={movie.title} rating={movie.rating} /> 
+      </>
+    )
+  });
+
   return (
     <div>
       <Header firstSrc={tabMovies[7]} secondSrc={tabMovies[5]} thirdSrc={tabMovies[8]} />
+      <div className="movies-nowplaying">
+        <div className="container row">
+          <div className="movies">
+            <h3 className="movies-title">A la une</h3>
+            <Link to="/film/populaire" className="movies-link">Voir tout</Link>
+          </div>
+          {moviesNowPlaying}
+        </div>
+      </div>
+
+      <div className="movies-toprated">
+        <div className="container row">
+          <div className="movies">
+            <h3 className="movies-title">Bien noté</h3>
+            <Link to="/film/populaire" className="movies-link">Voir tout</Link>
+          </div>
+          {moviesTopratedList}
+        </div>
+      </div>
+
       <div className="movies-popular">
         <div className="container row">
           <div className="movies">
@@ -43,15 +75,6 @@ const Home = () => {
             <Link to="/film/populaire" className="movies-link">Voir tout</Link>
           </div>
           {moviesPopularList}
-        </div>
-      </div>
-      <div className="movies-toprated">
-        <div className="container row">
-          <div className="movies">
-            <h3 className="movies-title">A la une</h3>
-            <Link to="/film/populaire" className="movies-link">Voir tout</Link>
-          </div>
-          {moviesTopratedList}
         </div>
       </div>
     </div>
--- src/service/api.jsx
+++ src/service/api.jsx
@@ -4,9 +4,10 @@ const apiKey = "8e75444d164c6753a09db0ea4696aae7";
 const apiUrl = "https://api.themoviedb.org/3";
 const nowPlayingUrl = `${apiUrl}/movie/now_playing`;
 const topratedMoviesUrl = `${apiUrl}/movie/top_rated`;
+const popularMoviesUrl = `${apiUrl}/movie/popular`;
 const movieUrl = `${apiUrl}/movie`;
 const movieCategoryUrl = `${apiUrl}/genre/movie/list`;
-const discoverMoviesUrl = `${apiUrl}/discover/movie`;
+const discoverMoviesUrl = `${apiUrl}/movie/discover`;
 const personUrl = `${apiUrl}/trending/person/week`;
 
 export const RecoverMovies = async () => {
@@ -84,6 +85,56 @@ export const RecoverPersons = () => {
 export const RecoverTopratedMovies = async () => {
     try {
         const {data} = await axios.get(topratedMoviesUrl, {
+            params:{
+                api_key:apiKey,
+                language:'en_US',
+                page:1
+            }
+        })
+        const posterUrl = 'https://image.tmdb.org/t/p/original/'
+        const modifiedData = data['results'].map((resultData) => ({
+            id: resultData['id'],
+            backPoster: posterUrl + resultData['backdrop_path'],
+            popularity: resultData['popularith'],
+            title: resultData['title'],
+            poster: posterUrl + resultData['poster_path'],
+            overview: resultData['overview'],
+            rating: resultData['vote_average']
+        }))
+        console.log("Les films au",modifiedData);
+        return modifiedData;
+    } catch (error) {
+        
+    }
+}
+export const RecoverPopularMovies = async () => {
+    try {
+        const {data} = await axios.get(popularMoviesUrl, {
+            params:{
+                api_key:apiKey,
+                language:'en_US',
+                page:1
+            }
+        })
+        const posterUrl = 'https://image.tmdb.org/t/p/original/'
+        const modifiedData = data['results'].map((resultData) => ({
+            id: resultData['id'],
+            backPoster: posterUrl + resultData['backdrop_path'],
+            popularity: resultData['popularith'],
+            title: resultData['title'],
+            poster: posterUrl + resultData['poster_path'],
+            overview: resultData['overview'],
+            rating: resultData['vote_average']
+        }))
+        console.log("Les films au",modifiedData);
+        return modifiedData;
+    } catch (error) {
+        
+    }
+}
+export const RecoverDiscoverMovies = async () => {
+    try {
+        const {data} = await axios.get(discoverMoviesUrl, {
             params:{
                 api_key:apiKey,
                 language:'en_US',
